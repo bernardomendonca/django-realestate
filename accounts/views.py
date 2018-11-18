a6889f29ab8d4d297458f85a7070aef07b65430a
bernardomendonca <bernardomendonca@outlook.com>
2018-11-19 10:55:34 +1100

    added register method to views

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,38 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 
 def register(request):
     if request.method == 'POST':
-        messages.error(request, 'Testing error message')
-        return redirect('register')
+        #Get form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # Check if passwords match
+        if password == password2:
+            #  Check username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'The username already exists')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'The email already exists')
+                    return redirect('register')
+                else:
+                    # Everything passed
+                    user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+                    # Login after register
+                    user.save()
+                    messages.success(request, 'You are now registered and can Log In')
+                    return redirect('login')
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
