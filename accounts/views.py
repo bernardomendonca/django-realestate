f492e25bd951bee06ab65bab6e173b5ad27d8ac0
bernardomendonca <bernardomendonca@outlook.com>
2018-11-19 10:38:27 +1100

    added message alert

---
+++ accounts/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+
+def register(request):
+    if request.method == 'POST':
+        messages.error(request, 'Testing error message')
+        return redirect('register')
+    else:
+        return render(request, 'accounts/register.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        #LOGIN USER
+        return
+    else:
+        return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    return redirect('index')
+
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
