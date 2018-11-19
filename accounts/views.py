00c42a979e26f684fc8e6b38e3539cb773fa5a27
bernardomendonca <bernardomendonca@outlook.com>
2018-11-19 14:19:42 +1100

    created the contacts model

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
 from django.contrib.auth.models import User
 
 
@@ -39,14 +39,27 @@ def register(request):
 
 def login(request):
     if request.method == 'POST':
-        #LOGIN USER
-        return
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid credentials')
+            return redirect('login')
     else:
         return render(request, 'accounts/login.html')
 
 
 def logout(request):
-    return redirect('index')
+    if request.method == "POST":
+        auth.logout(request)
+        messages.success(request, 'You are now logged out')
+        return redirect('login')
 
 
 def dashboard(request):
