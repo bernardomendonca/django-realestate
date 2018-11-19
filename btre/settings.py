6469d0a4ab2e0ae60d23a6dcd55144342e9256ff
bernardomendonca <bernardomendonca@outlook.com>
2018-11-19 16:05:00 +1100

    created dynamic dashboard

--- btre/settings.py
+++ btre/settings.py
@@ -141,3 +141,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+# EMAIL CONFIG
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER =''
+EMAIL_HOST_PASSWORD=''
+EMAIL_USE_TLS=True
